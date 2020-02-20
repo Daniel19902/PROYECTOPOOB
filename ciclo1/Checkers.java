8da82d5a4173ab8b00256c68c7437aca1f33c48c
unknown <edward.porras@labinfo.is.escuelaing.edu.co>
2020-02-20 16:36:16 -0500

    ciclo2

--- ciclo1/Checkers.java
+++ ciclo1/Checkers.java
@@ -11,10 +11,15 @@ public class Checkers
     private Rectangle[][] zonaConfiguracion;
     private Rectangle[][] zonaJuego;
     private Ficha[] listaDeFichas;
-    private int seleccionRow;
-    private int seleccionColumn;
+    private static int seleccionRow;
+    private static int seleccionColumn;
     private Rectangle[] listaDePosicionlesNegras;
-    
+    private char movimiento;
+    /**
+     * Checkers realiza el tablero de configuracion y de juego
+     * @param width un entero para determinar el tamaño del tablero (width*width)
+     */
+   
     public Checkers(int width){
         listaDeFichas = new Ficha[33];
         listaDePosicionlesNegras = new Rectangle[33];
@@ -38,6 +43,11 @@ public class Checkers
             }
         }
     }
+    /**
+     * selecciona una ficha del tablero
+     * @param row fila del tablero
+     * @param column columna del tablero
+     */
     public void select(int row, int column){
         seleccionRow = row;
         seleccionColumn = column;
@@ -45,6 +55,11 @@ public class Checkers
         Ficha fichaSeleccionada = casillaSeleccionada.getFicha();
         fichaSeleccionada.changeColor("red");
     }
+    /**
+     * realiza un movimiento en diagonal
+     * @param top si top == false la ficha va asi arriva y si no va asi abajo
+     * @param right si right == false la ficha va asi la izquierda si no va asi la derecha
+     */
     public void shift(boolean top, boolean right){
         Rectangle casillaSeleccionada = zonaConfiguracion[seleccionRow][seleccionColumn];
         Ficha fichaSeleccionada = casillaSeleccionada.getFicha();
@@ -64,6 +79,11 @@ public class Checkers
         casillaAMover.asignarFicha(fichaSeleccionada);fichaSeleccionada.changeColor("blue");
         casillaSeleccionada.asignarFicha(null);
     }
+    /**
+     * realiza saltos que elimina fichas del juego
+     * @param top si top == false la ficha va asi arriva y si no va asi abajo
+     * @param right si right == false la ficha va asi la izquierda si no va asi la derecha
+     */
     public void jump(boolean top, boolean right){
         System.out.println("yo jump");
         Rectangle casillaSeleccionada = zonaConfiguracion[seleccionRow][seleccionColumn];
@@ -72,43 +92,58 @@ public class Checkers
         if (top && right){
             fichaAMatar = zonaConfiguracion[seleccionRow+1][seleccionColumn+1];
             casillaALlegar = zonaConfiguracion[seleccionRow+2][seleccionColumn+2];
+            seleccionRow = seleccionRow+2;seleccionColumn = seleccionColumn+2;
         }else if (top && !right){
             fichaAMatar = zonaConfiguracion[seleccionRow+1][seleccionColumn-1];
             casillaALlegar = zonaConfiguracion[seleccionRow+2][seleccionColumn-2];
+            seleccionRow = seleccionRow+2;seleccionColumn = seleccionColumn-2;
         }else if (!top && right){
             fichaAMatar = zonaConfiguracion[seleccionRow-1][seleccionColumn+1];
             casillaALlegar = zonaConfiguracion[seleccionRow-2][seleccionColumn+2];
+            seleccionRow = seleccionRow-2;seleccionColumn = seleccionColumn+2;
         }else if (!top && !right){
             fichaAMatar = zonaConfiguracion[seleccionRow-1][seleccionColumn-1];
             casillaALlegar = zonaConfiguracion[seleccionRow-2][seleccionColumn-2];
+            seleccionRow = seleccionRow-2;seleccionColumn = seleccionColumn-2;
         }
         if (fichaAMatar.getFicha()!=null){
                 fichaAMatar.getFicha().makeInvisible();
                 fichaAMatar.asignarFicha(null);
+                System.out.println(casillaSeleccionada.getFicha());
                 casillaSeleccionada.getFicha().moveAXyMoverAY(casillaALlegar.getPosicionX(),casillaALlegar.getPosicionY());
                 casillaALlegar.asignarFicha(casillaSeleccionada.getFicha());
+                casillaSeleccionada.asignarFicha(null);
                 casillaALlegar.getFicha().changeColor("blue");
-        }
+            }
     }
+    /**
+     * es la combinacion del shift y el jump
+     * @param notacion representa las posciones y el tipo de movimiento que se va arelaizar
+     */
+    
     public void move(String notacion){
         boolean top = true;
         ArrayList<Integer> move = passToStringToInt(notacion);
         boolean right = true;
-        //right = move(move.get(0),move.get(1));
-        //if (move.get(0) > move.get(1))top = false;
-        //else top = true;
-        //shift(top,right);
-        for (int i = 0; (int) i<(move.size()/2); i++){
-            if(i%2 == 0){
+        if (this.movimiento == '-'){
+            right = move(move.get(0),move.get(1));
+            if (move.get(0) > move.get(1))top = false;
+            else top = true;
+            shift(top,right);
+        }
+        else{
+            for (int i = 0; (int) i<(move.size()-1); i++){
                 if (move.get(i) > move.get(i+1)) top = false;
                 else top = true;
-                jump(move.get(i),move.get(i+1));
+                right = jump(move.get(i),move.get(i+1));
+                System.out.println("yo"+" "+top);
+                jump(top,right);
             }
-            System.out.println("yo"+" "+top);
-            jump(top,right);
         }
     }
-    
+    /**
+     * ase el tablero configuracion y juego visible
+     */
     public void makeVisible(){
         for (int i = 0; i < zonaConfiguracion.length; i++){
             for (int j = 0; j < zonaConfiguracion.length; j++){
@@ -117,6 +152,10 @@ public class Checkers
             }
         }
     }
+    /**
+     * añade fichas 
+     * @param men una matriz de posiciones
+     */
     public void add(int[][] men){
         for (int i = 0; i< men.length; i++){
             Rectangle asignarFicha = zonaConfiguracion[men[i][0]][men[i][1]];
@@ -130,6 +169,12 @@ public class Checkers
             }   
         }
     }
+    /**
+     * añade una ficha "rey" o "reina"
+     * @param king si king es false es rey y si no es reina
+     * @param row una fila del tablero
+     * @param coulm una columna del tablero
+     */
     public void add(boolean king, int row, int column){
         Rectangle asignarFicha = zonaConfiguracion[row][column];
         if (asignarFicha.esColocarFicha()){
@@ -140,15 +185,28 @@ public class Checkers
             JOptionPane.showMessageDialog(null,"ya hay una ficha en esa posición");
         }
     }
+    /**
+     * remoeve una ficha del tablero
+     * @param row una fila del tablero
+     * @param coulm una columna del tablero
+     */
     public void remove(int row, int column){
         zonaConfiguracion[row][column].getFicha().makeInvisible();
         zonaConfiguracion[row][column].asignarFicha(null);
     }
+    /**
+     * remueve fichas del tablero
+     * @param pieces una matriz de posiciones
+     */
     public void remove(int[][] pieces){
         for(int i = 0; i< pieces.length; i++){
             remove(pieces[i][0],pieces[i][1]);
         }
     }
+    /**
+     * pasa de String a entero
+     * @param notacion una cadena para covertir de String a entero
+     */
     private ArrayList<Integer> passToStringToInt(String notacion){
         ArrayList<Integer> move;
         move = new ArrayList<Integer>();
@@ -160,19 +218,22 @@ public class Checkers
         if (letra == '-' || letra2 == '-'){
             move.add(Integer.parseInt(shift[0]));
             move.add(Integer.parseInt(shift[1]));
+            
         }else{
             for(int i = 0; i< jumps.length; i++){
                  move.add(Integer.parseInt(jumps[i]));
             }
         }
+        if (letra == '-' || letra == 'x') this.movimiento = letra;
+            else this.movimiento = letra2;
         return move;
         //fin de convertir String a int
     }
-    private int getPositionRectangleY(int row, int column){
-        Rectangle posicionY = zonaConfiguracion[row][column];
-        int y = posicionY.getPosicionY();
-        return y;
-    }
+    /**
+     * determina el movimiento shift
+     * @param shift un entero que representa una cuadrado del tablero
+     * @param shift2 un entero que representa una cuadrado del tablero
+     */
     private boolean move(int shift, int shift2){
         boolean right;
         double decimal = (double)shift/4;
@@ -201,6 +262,11 @@ public class Checkers
         }
         return right;
     }
+    /**
+     * determina el movimiento jump
+     * @param jump un entero que representa una cuadrado del tablero
+     * @param jump2 un entero que representa una cuadrado del tablero
+     */
     private boolean jump(int jump, int jump2){
         System.out.println("yo");
         boolean right = true;
